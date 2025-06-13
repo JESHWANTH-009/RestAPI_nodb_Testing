188303430daba0817eb29cb4abc69af50c52f0c1
JESHWANTH-009 <jeshwanth912@gmail.com>
2025-06-13 12:12:40 +0530

    Update REST_nodb

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FASTAPI,HTTPException 
+from pip._internal.cli import status_codes
+
+app=FASTAPI()
+
+tasks=[{"id":1,"title":"Learn REST",'complted':True},
+       {"id":2,"title":"learn AWS","completed":True},
+       "id":3,"title":"learn LAngchain","completed":True]
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{tasks_id}")
+def get_tasks(tasks_id:int): ## defining type of input id 
+    for i in tasks:
+        if i["id"]==tasks_id:
+            return i
+    raise HTTPException(status_code=404,detail="Task not found")
+
+@app.post("/tasks")
+def create_task(task:dict):
+    task["id"]=len(tasks)+1
+    tasks.append(task)
+    return task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id:int,new_task:dict):
+    for i in tasks:
+        if i["id"]==task_id:
+            i.update(new_task)  ## to update dictonary
+            return i
+    raise HTTPException(status_code=404,detail="Task not found")
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id:int):
+    for index, task in enumerate(tasks):
+        if task["id"]==task_id:
+            deleted_task=tasks.pop(index)
+            return deleted_task
+    raise HTTPException(status_code=404, detail="Task not found")
+    
